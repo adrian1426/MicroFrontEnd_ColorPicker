9f01c810821f15b2cc0585bbb248aaf9e942be4f
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-04-18 10:33:22 -0600

    host app -agregando bootstrap css

--- host-app/src/App.jsx
+++ host-app/src/App.jsx
@@ -4,11 +4,6 @@ import ColorPicker from "colorPicker/ColorPicker";
 
 const App = () => (
   <div className="container">
-    <div>Name: host-app</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
-    <br />
     <ColorPicker />
   </div>
 );
