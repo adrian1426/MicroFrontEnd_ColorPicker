9c4bd67812d25d8c25fe5ad2b8258940e807ddd6
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-04-18 12:05:02 -0600

    agregando y estilizando color picker component

--- host-app/src/App.jsx
+++ host-app/src/App.jsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom";
 import ColorPicker from "colorPicker/ColorPicker";
 
 const App = () => (
-  <div className="container">
-    <ColorPicker />
-  </div>
+  <>
+    <h1 className="text-center bg-dark text-white p-2">Color Picker</h1>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-12 col-md-4">Lista de colores</div>
+        <div className="col-12 col-md-8">
+          <ColorPicker />
+        </div>
+      </div>
+    </div>
+  </>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
