1255f3be3dd12a2ea6cf32c20b9b59b44827f69a
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-04-19 15:27:28 -0600

    mf-color-list - se estiliza frontend color list

--- mf-color-list/src/components/ColorList.jsx
+++ mf-color-list/src/components/ColorList.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const ColorList = () => {
+
+  const colors = ["#000000", "#971c1c", "#2ab4cf", "#4ecf2a"];
+
   return (
-    <div>
-      ColorList component
+    <div className="list-group text-center">
+      {
+        colors.map((color, i) => (
+          <button
+            key={i}
+            type='button'
+            className='list-group-item list-group-item-action text-primary text-white'
+            aria-current="true"
+            title='Copiar'
+            style={{
+              background: color,
+              fontWeight: 'bolder'
+            }}
+          >
+            {color}
+          </button>
+        ))
+      }
     </div>
   );
 };
